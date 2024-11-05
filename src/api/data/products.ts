fe27e3687f23d3f383466f7bedbd8af0071b4976
TheoDieppois <theo76.dieppois@gmail.com>
2024-11-05 20:30:33 +0100

    feat: add product file with mock data

---
+++ src/api/data/products.ts
@@ -0,0 +1,66 @@
+type Product = {
+  id: number;
+  name: string;
+  category: "sport" | "food" | "tech";
+  price: number;
+  image: string;
+};
+
+export const Products: Product[] = [
+  {
+    id: 1,
+    name: "Produit 1",
+    category: "sport",
+    price: 100,
+    image: "https://via.placeholder.com/150",
+  },
+  {
+    id: 2,
+    name: "Produit 2",
+    category: "food",
+    price: 200,
+    image: "https://via.placeholder.com/150",
+  },
+  {
+    id: 3,
+    name: "Produit 3",
+    category: "tech",
+    price: 300,
+    image: "https://via.placeholder.com/150",
+  },
+  {
+    id: 4,
+    name: "Produit 4",
+    category: "sport",
+    price: 400,
+    image: "https://via.placeholder.com/150",
+  },
+  {
+    id: 5,
+    name: "Produit 5",
+    category: "food",
+    price: 500,
+    image: "https://via.placeholder.com/150",
+  },
+  {
+    id: 6,
+    name: "Produit 6",
+    category: "tech",
+    price: 600,
+    image: "https://via.placeholder.com/150",
+  },
+  {
+    id: 7,
+    name: "Produit 7",
+    category: "sport",
+    price: 700,
+    image: "https://via.placeholder.com/150",
+  },
+  {
+    id: 8,
+    name: "Produit 8",
+    category: "food",
+    price: 800,
+    image: "https://via.placeholder.com/150",
+  },
+];
