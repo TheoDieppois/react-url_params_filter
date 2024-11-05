c5f0408d973bee52adb499b9ce257aabc246192d
TheoDieppois <theo76.dieppois@gmail.com>
2024-11-05 23:25:25 +0100

    feat: set up initial route for HomePage with createBrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import HomePage from "./pages/HomePage.tsx";
+import "./index.css";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+]);
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} />
     </QueryClientProvider>
-  </StrictMode>
+  </StrictMode>,
 );
